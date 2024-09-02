a45c392aa0d6ee5e076f2cd30a4c001ba4d6a819
maidey <maideyildizz@gmail.com>
2024-09-02 07:48:28 +0300

    credentials added service edit

--- main/ExpenseTracker.Core/Repositories/IBaseRepository.cs
+++ main/ExpenseTracker.Core/Repositories/IBaseRepository.cs
@@ -4,7 +4,7 @@ namespace ExpenseTracker.Core.Repositories;
 public interface IBaseRepository<T> where T : Base
 {
     Task<IEnumerable<T>> GetAllAsync();
-    Task<T> GetByIdAsync(int id);
+    Task<T?> GetByIdAsync(int id);
     Task AddAsync(T obj);
     Task UpdateAsync(T obj);
     Task DeleteAsync(int id);
