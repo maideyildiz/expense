1f16d5515af165e50fa0f5d54a18a863b440a151
maidey <maideyildizz@gmail.com>
2024-09-15 11:17:41 +0300

    config edit

---
+++ main/ExpenseTracker.Infrastructure/Services/ConfigReader.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ExpenseTracker.Infrastructure.Services
+{
+    public class ConfigReader
+    {
+        private readonly string _filePath;
+
+        public ConfigReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public JObject ReadConfig()
+        {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException("Config file not found", _filePath);
+
+            string json = File.ReadAllText(_filePath);
+            return JObject.Parse(json);
+        }
+    }
+}
