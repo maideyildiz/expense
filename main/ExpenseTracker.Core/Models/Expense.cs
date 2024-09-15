619bb9b8263192592d42b314635528288b964950
maidey <maideyildizz@gmail.com>
2024-09-15 10:59:49 +0300

    table relations

--- main/ExpenseTracker.Core/Models/Expense.cs
+++ main/ExpenseTracker.Core/Models/Expense.cs
@@ -3,6 +3,7 @@ namespace ExpenseTracker.Core.Models;
 public class Expense : Base
 {
     public string Note { get; set; } = string.Empty;
-    public required Category Category { get; set; }
     public decimal Amount { get; set; }
+    public int CategoryId { get; set; }
+    public required Category Category { get; set; }
 }
