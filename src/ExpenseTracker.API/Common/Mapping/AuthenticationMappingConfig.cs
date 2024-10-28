be56d48925ab7c1748eeb028973a429c4b7df5e2
Maide Yıldız <maideyildizz@gmail.com>
2024-10-28 23:12:45 +0300

    ++

--- src/ExpenseTracker.API/Common/Mapping/AuthenticationMappingConfig.cs
+++ src/ExpenseTracker.API/Common/Mapping/AuthenticationMappingConfig.cs
@@ -1,6 +1,8 @@
 namespace ExpenseTracker.API.Common.Mapping;
 
+using ExpenseTracker.Application.Authentication.Commands.Register;
 using ExpenseTracker.Application.Authentication.Common;
+using ExpenseTracker.Application.Authentication.Queries.Login;
 using ExpenseTracker.Contracts.Authentication;
 
 using Mapster;
@@ -8,6 +10,8 @@ public class AuthenticationMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+        config.NewConfig<LoginRequest, LoginQuery>();
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
         .Map(dest => dest, src => src.User);
     }
