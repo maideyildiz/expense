dd08952b8f8b1c1f8a4ca3e801f5d8a6f47a8307
Maide Yıldız <maideyildizz@gmail.com>
2024-10-29 23:31:08 +0300

    controllers added

--- src/ExpenseTracker.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ src/ExpenseTracker.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -5,7 +5,7 @@ using ExpenseTracker.Application.Common.Interfaces.Persistence;
 
 public interface IJwtTokenGenerator
 {
-    string GenerateToken(Guid id, string name, string surname);
+    string GenerateToken(Guid id, string name, string surname, string subscriptionName = "");
     ClaimsPrincipal? ValidateToken(string token);
     string RevokeToken(string token);
 }
