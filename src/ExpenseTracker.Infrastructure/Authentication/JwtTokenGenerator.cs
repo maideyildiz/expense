9f098e10f44631440a486607a086f061b2e00482
Maide Yıldız <maideyildizz@gmail.com>
2024-11-10 15:10:10 +0300

    ++

--- src/ExpenseTracker.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ src/ExpenseTracker.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -28,7 +28,6 @@ public class JwtTokenGenerator : IJwtTokenGenerator
             new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
             new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            //new Claim(ClaimTypes.Role, user.SubscriptionId.ToString()!), // Add user roles if necessary
         };
 
         var tokenDescriptor = new JwtSecurityToken(
